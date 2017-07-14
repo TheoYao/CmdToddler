9aee3f5fbe8fb373900b4ed1ea8fc4ee9a49b984
TheoYao <TheoYao@163.com>
2017-07-14 22:53:26 +0800

    lets start

---
+++ 1_1_include.cpp
@@ -0,0 +1,11 @@
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <GLUT/glut.h>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    cout<< "wow"<< endl;
+    return 0;
+}
