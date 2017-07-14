c4fb9e164b4374d1c243e1b574f7f272ccbd65f4
TheoYao <TheoYao@163.com>
2017-07-14 23:33:58 +0800

    oops first window!

---
+++ 1_2_first_window.cpp
@@ -0,0 +1,60 @@
+// #include <iostream>
+// #include <GL/glew.h>
+// #include <GLFW/glfw3.h>
+// 
+// using namespace std;
+// 
+// int main(int argc, char **argv) {
+//     glfwInit();
+//     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+//     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+//     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+// 
+//     return 0;
+// }
+// 
+#include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+void Render(void)
+{
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_TRIANGLES);
+    {
+        glColor3f(1.0,0.0,0.0);
+        glVertex2f(0, .5);
+        glColor3f(0.0,1.0,0.0);
+        glVertex2f(-.5,-.5);
+        glColor3f(0.0, 0.0, 1.0);
+        glVertex2f(.5, -.5);
+    }
+    glEnd();
+}
+
+int main(int argc, const char * argv[]) {
+    GLFWwindow* win;
+    if(!glfwInit()){
+        return -1;
+    }
+    win = glfwCreateWindow(640, 480, "OpenGL Base Project", NULL, NULL);
+    if(!win)
+    {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+    if(!glewInit())
+    {
+        return -1;
+    }
+    glfwMakeContextCurrent(win);
+    while(!glfwWindowShouldClose(win)){
+        Render();
+        glfwSwapBuffers(win);
+        glfwPollEvents();
+    }
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
+    return 0;
+}
